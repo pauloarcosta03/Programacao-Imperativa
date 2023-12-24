e7571efc4c18aac323af303f0334bb3061d0b44e
pauloarcosta03 <94547074+pauloarcosta03@users.noreply.github.com>
2023-12-24 00:50:27 +0000

    Fix Cabeçalho

--- TrabalhoPratico1/Source.c
+++ TrabalhoPratico1/Source.c
@@ -1,6 +1,13 @@
 /*
 * Paulo Alexandre Rodrigues da Costa
 * a29851@alunos.ipca.pt
+* 
+* Afonso Teixeira Ferreira
+* a27981@alunos.ipca.pt
+* 
+* David João Martins Lopes
+* a26017@alunos.ipca.pt
+* 
 * 22/11/2023
 * 
 * Trabalho Prático 1
