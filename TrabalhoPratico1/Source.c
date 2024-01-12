0391daae176e1efbfd0143004dd9b81e3a45dc4d
pauloarcosta03 <94547074+pauloarcosta03@users.noreply.github.com>
2024-01-12 12:41:27 +0000

    fix makefile

--- TrabalhoPratico1/Source.c
+++ TrabalhoPratico1/Source.c
@@ -19,5 +19,35 @@
 #include <stdio.h>
 
 int main() {
+    Paciente pacientes[N];
+    Plano planos[N];
+    Dieta dietas[N];
+    MediaCalorias mediacal[N];
 
+    ImportarPacientes(pacientes, "Pacientes1.csv");
+
+    ImportarPlanos(planos, "Planos1.csv", pacientes);
+
+    DietaPaciente(dietas, "Dietas1.csv");
+
+    /*int result = NumPacientesPassaLim(dietas, 4, 119, dietas[2].data);
+
+    result;
+
+    Paciente pacientesOrd[N];
+
+    ListaPacientesCalMais(planos, 3, dietas, 4, pacientes, 3, pacientesOrd);
+
+    pacientesOrd;
+
+    Plano detalhes[N];
+    ListarPlanoPorRefeicao(planos, detalhes, 3, 1, PequenoAlmoco, ConverteDataTimet("1/1/2023"), ConverteDataTimet("2/1/2023"));
+
+    detalhes;
+
+    MediaCalorias mc[N];
+    calcularMediaCalorias(dietas, 4, pacientes, 3, PequenoAlmoco, mc, ConverteDataTimet("1/1/2023"), ConverteDataTimet("2/1/2023"));
+    mc;*/
+
+    TabelaDietas(dietas, 4, pacientes, 3, planos, 3);
 }
